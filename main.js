b0c9e2c5cab7140b1c767ffce812945f7bf107a9
AllanLimitless <allanlimitless55@gmail.com>
2020-03-28 09:32:01 -0400

    Update
    Adding start/stop button functionality

--- main.js
+++ main.js
@@ -15,54 +15,46 @@
 // you can clear/restart
 
   
-// document.getElementById("dl").addEventListener("click", handler()); 
-
-function handler() {
+// function handler() {
 
   //taking user input
-  var time1 = document.getElementById('hms').value;
+  let time1 = document.getElementById('hms').value;
   //splitting it to seperate variables
-  var pieces = time1.split(":");
-
-  var hours = pieces[0];
-  var minutes = pieces[1];
-  var seconds = pieces[2];
-  var time = {
+  let pieces = time1.split(":");
+  let hours = pieces[0];
+  let minutes = pieces[1];
+  let seconds = pieces[2];
+  let time = {
       hours: hours * 1,
       minutes: minutes * 1,
       seconds: seconds * 1
   };
+//* Save the interval's handle to `timer`
+//* The setInterval() method repeats a given function at every given time-interval.
+let myTimer;
 
+function newTimer(){
+ myTimer = setInterval(countdown, 1000);
+}
 
-  // Save the interval's handle to `timer`
-  var timer = setInterval(countdown, 1000);
-
-  function countdown() {
-      var container = document.getElementById('count');
-
+function countdown() {
+    let container = document.getElementById('count');
       if (time.seconds > 0) {
           time.seconds--;
-      }
-      else {
+      } else {
           if (time.minutes > 0) {
               time.minutes--;
               time.seconds = 60;
-          }
-          else {
+          } else {
               time.minutes = 60;
               time.seconds = 60;
               time.hours--;
           }
       }
-
       if (time.hours >= 0) {
           container.innerHTML = time.hours + ':' + time.minutes + ':' + time.seconds;
-      }
-      else {
+      } else {
           container.innerHTML = 'Time over';
-          clearInterval(timer);
+          clearInterval(myTimer);
       }
   }
-  
-}
-
