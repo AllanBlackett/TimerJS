b44292f2bb1a74f74ada0200e17b6ed7a10563b7
AllanLimitless <allanlimitless55@gmail.com>
2020-04-02 07:58:43 -0400

    main.js update
    Finishing touches

--- main.js
+++ main.js
@@ -1,89 +1,52 @@
-//* The Idea
-// Stopwatch
-// Starts at 0
-// press play to start the timer 
-// you can pause the stopwatch timer and resume
-// you can reset the stopwatch back to 0 
+// Count starts at 0
+// press start() to begin the timer 
+// you can stop() the stopwatch timer and start() to resume
+// you can reset() the stopwatch back to 0 to reset the timer
 
+let hour = 0;
+let minute = 0;
+let seconds = 0;
+let c = 0;
+let t = null;
+let timer = 0;
 
+function countUp() {
+  ++c;
+  t = setTimeout(countUp, 1000);
 
+  hour = Math.floor(c / 3600);
+  minute = Math.floor((c - hour * 3600) / 60);
+  seconds = c - (hour * 3600 + minute * 60);
 
-// Timer 
-// placeholder input number 
-// play to start the countdown
-// you can pause and resume 
-// you can clear/restart
+  document.getElementById('hour').innerHTML = hour + ':';
+  document.getElementById('minute').innerHTML = minute + ':';
+  document.getElementById('seconds').innerHTML = seconds;
+}
 
-  
-// function handler() {
-
-  //taking user input
-  let time1 = document.getElementById('hms').value;
-  //splitting it to seperate variables
-  let pieces = time1.split(":");
-  let hours = pieces[0];
-  let minutes = pieces[1];
-  let seconds = pieces[2];
-  let time = {
-      hours: hours * 1,
-      minutes: minutes * 1,
-      seconds: seconds * 1
-  };
-//* Save the interval's handle to `timer`
-//* The setInterval() method repeats a given function at every given time-interval.
-let myTimer;
-
-// Start Timer
-function newTimer(){
- myTimer = setInterval(countdown, 1000);
- document.getElementById('dl').classList.add('hide');
-} 
-
-
-// function stopTimer() {
-//     document.getElementById('dl').classList.add('show');
-// }
-
-// document.getElementById('sl').onclick = stopTimer();
-
-
-function countdown() {
-    let container = document.getElementById('count');
-      if (time.seconds > 0) {
-          time.seconds--;
-      } else {
-          if (time.minutes > 0) {
-              time.minutes--;
-              time.seconds = 60;
-          } else {
-              time.minutes = 60;
-              time.seconds = 60;
-              time.hours--;
-          }
-      }
-      if (time.hours >= 0) {
-          container.innerHTML = time.hours + ':' + time.minutes + ':' + time.seconds;
-      } else {
-          container.innerHTML = 'Time over';
-          clearInterval(myTimer);
-          document.getElementById('dl').classList.add('show');
-
-      }
+// Start
+function start() {
+  // Set timer to false so when user do clearTimeout, start() is ready 
+    // to get called again and without the capability of making repeat 
+    // calls to the start() function. 
+  if (!timer) {
+    timer = 1;
+    countUp();
   }
-// ! Something that refreshes or restarts the "state of everything"
-// ! If I can just reset everything to a "blank state" that works
-  function restartTimer() {
-      if (myTimer) {
-          clearInterval(myTimer);
-        //   document.getElementById('count').innerHTML = "0:0:0";
-          document.getElementById('dl').classList.add('show');
-          document.getElementById('hms').innerHTML = "0:0:0";
-        //   time.seconds = 0;
-        //   time.minutes = 0;
-        //   time.hours = 0;
-      } else {
-        // myTimer = setInterval(countdown, 1000);
-      }
+}
+
+// Stop
+function stop() {
+  if (t) {
+    clearTimeout(t);
+    timer = 0;
   }
+}
 
+// Restart
+function restart() {
+  c = 0;
+  document.getElementById('hour').innerHTML = '0';
+  document.getElementById('minute').innerHTML = '0';
+  document.getElementById('seconds').innerHTML = '0';
 
+}
